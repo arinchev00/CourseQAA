70c9d8863933b5bc33460a70ba3313d25dd05c78
Ilya <ilya.arinchev@simbirsoft.com>
2025-08-03 16:27:14 +0300

    3 commit

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -3,8 +3,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.xpath;
 
 public class TextBoxTests {
     @BeforeAll
@@ -18,14 +20,31 @@ public class TextBoxTests {
         String firstName = "Илья";
         String lastName = "Аринчев";
         String userEmail = "arinchev00@rambler.ru";
+        String userNumber = "8927850610";
+        String subjects = "Maths";
+        String file = "png/Скрин.png";
+        String currentAddress = "Адрес";
 
         open("https://demoqa.com/automation-practice-form");
 
 
-        $("[id=firstName]").setValue(firstName);
-        $("[id=lastName]").setValue(lastName);
-        $("[id=userEmail]").setValue(userEmail);
-        $("[id=submit]").scrollIntoView(true).click();
-        $("[id=submit]").scrollIntoView(true).click();
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("label[for='gender-radio-1']").click();
+        $("#userNumber").setValue(userNumber);
+        $("[id=dateOfBirthInput]").click();
+        $(xpath("//div[contains(@aria-label,'10th')]"))
+                .click();
+        $("label[for='hobbies-checkbox-1']").click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#uploadPicture").uploadFromClasspath(file);
+        $("#currentAddress").setValue(currentAddress);
+        $("#stateCity-label").scrollIntoView(true);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("[id=submit]").click();
     }
 }
